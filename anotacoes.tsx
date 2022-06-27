575cb0ae492b315083e033c6afcfad4f534f6c4a
heliobarolo0586 <heliobarolo0586@gmail.com>
2022-06-27 13:15:41 -0300

    Subindo repositório do Projeto

---
+++ anotacoes.tsx
@@ -0,0 +1,22 @@
+// CMs = Content Management System
+
+// Traz tanto o painel de ADMIN tanto quanto a parte visual do front-end (temas)
+
+// Headless CMS (GraphCMS): Painel de ADMIN (dados fornecidos através de uma API REST ou GraphQL)
+// React que consome a API do CMS 
+
+// Métodos de consumir GraphQL
+// query / mutation 
+
+// query = buscar dados
+// mutation = criar, alterar ou deletar dados
+
+// Exemplo de query
+
+//   useEffect(() => {
+//     client.query({
+//       query: GET_LESSIONS_QUERY
+//     }).then(response => {
+//       console.log(response.data)
+//     })
+//   },[])
